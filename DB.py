cfb4e658598df304747907bb39958600eb9591e5
Nikita Panferov <nikpanfer@gmail.com>
2023-05-28 01:30:20 +0300

    Реализована работа с бд

--- DB.py
+++ DB.py
@@ -1,3 +1,4 @@
+
 from asyncpg import create_pool
 
 class Db:
@@ -16,23 +17,33 @@ class Db:
             database=name
         )
 
-
     async def get_last_recipe(self):
         async with self.pool.acquire() as conn:
-            return await conn.fetch(f'SELECT url FROM {self.recipe_table} order by id desc limit 1')
-
+            resp = await conn.fetch(f'SELECT name FROM {self.recipe_table} order by id desc limit 1')
+            return resp[0]['name'] if resp else ''
     async def _bulk_create_products(self, products):
+        query = f'''INSERT INTO {self.product_table} (name) VALUES ($1)  ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING id;'''
         async with self.pool.acquire() as conn:
-            return await conn.executemany(f'''INSERT INTO {self.product_table} (name) VALUES ($1)  ON CONFLICT (name) DO NOTHING RETURNING id;''', products)
+            results = []
+            for product in products:
+                result = await conn.fetch(query, product)
+                results.append(result[0]['id'])
+
+            return results
+            # return await conn.executemany(f'''INSERT INTO {self.product_table} (id, name) VALUES ($1, $2)  ON CONFLICT (name) DO NOTHING RETURNING id;''', products)
 
     async def _create_recipe(self, recipe):
         async with self.pool.acquire() as conn:
-            return await conn.fetch(
-                f'''INSERT INTO {self.recipe_table} (name, weight, calories, img) VALUES ($1, $2, $3, $4) RETURNING id;''', recipe)
+            resp = await conn.fetch(
+                f'''INSERT INTO {self.recipe_table} (name, weight, calories, img, url) VALUES ($1, $2, $3, $4, $5) RETURNING id;''', *recipe)
+            return resp[0]['id']
 
     async def new_recipe(self, recipe_data):
         recipe_id = await self._create_recipe(recipe_data[0])
         prods = await self._bulk_create_products(recipe_data[1])
         async with self.pool.acquire() as conn:
             for prod in zip(prods, recipe_data[2]):
-                await conn.execute(f'''INSERT INTO {self.product_recipe} (recipe_id, product_id, weight) VALUES ($1, $2, $3)''', (recipe_id, prod[0], prod[1]))
+                await conn.execute(f'''INSERT INTO {self.product_recipe} (recipe_id, product_id, weight) VALUES ($1, $2, $3)''', recipe_id, prod[0], prod[1])
+
+    async def disconnect(self):
+        await self.pool.close()
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import asyncio
-import sys
+import os
 
 from aiohttp import ClientSession
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
 from DB import Db
 
+load_dotenv()
+
 page_count = 64
+
+host = os.environ.get('DB_HOST', '127.0.0.1')
+port = os.environ.get('DB_PORT', '5432')
+user = os.environ.get('DB_USER', 'root')
+password = os.environ.get('DB_PASS', 'root')
+db_name = os.environ.get('DB_NAME', 'mm')
 
 
 async def get_item(url, name, session, db):
@@ -14,21 +23,23 @@ async def get_item(url, name, session, db):
         page = await response.text()
         bs = BeautifulSoup(page, 'lxml')
         table = bs.find('table', class_='recipe_calculation')
-        tfood = table.find('tfoot').find_all('td', class_='variable')
+        tfoot = table.find('tfoot').find_all('td', class_='variable')
         img = bs.find('img', class_='recipe_image_main')
-        recipe = (name, tfood[0].get_text(), tfood[1].get_text(), img.get('src'))
+        recipe = (name, float(tfoot[0].get_text()), float(tfoot[1].get_text()), f"https://daily-menu.ru{img.get('src')}", url)
         items = table.find('tbody').find_all('tr')
         prods = [it.find('td').get_text() for it in items]
-        weight = [(it.find('td', class_='variable').get_text(),) for it in items]
-        # await db.new_recipe(recipe, prods, weight)
-        print(recipe, prods, weight)
+        weight = [float(it.find('td', class_='variable').get_text()) for it in items]
+        await db.new_recipe((recipe, prods, weight))
+        # print(recipe, prods, weight)
+
 
 async def main():
-    # db = Db()
-    db = ''
-    # await db.connect()
-    # last_title = await db.get_last_recipe()
-    last_title = ''
+    db = Db()
+    # db = ''
+    await db.connect(host, port, user, password, db_name)
+    last_title = await db.get_last_recipe()
+    print(last_title)
+    # last_title = ''
     if_exit = False
     async with ClientSession() as session:
         for i in range(page_count):
@@ -41,10 +52,12 @@ async def main():
                 if name == last_title:
                     if_exit = True
                     break
-                tasks.append(asyncio.create_task(get_item(f'https://daily-menu.ru/{item.find("a").get("href")}', name, session, db)))
+                tasks.append(asyncio.create_task(get_item(f'https://daily-menu.ru{item.find("a").get("href")}', name, session, db)))
             await asyncio.gather(*tasks)
+            # print(f'added {25 * i+1} recipes')
             if if_exit:
                 break
+    await db.disconnect()
 
 
 asyncio.run(main())
